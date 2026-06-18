c44cb94e7005b3724a70407f4e1f6535d1996881
micheleferri01 <micheleferri095@gmail.com>
2026-06-18 21:43:34 +0200

    update: view show of route projects

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
 <div class='container'>
-    <h1 class='my-3'>{{$project->name}}</h1>
-    
-    <h2 class='fs-4'>Client: {{$project->client}}</h2>
-    
+    <div class='my-3'>
+        <h1>{{$project->name}}</h1>
+        <h2>- {{$project->author}}</h2>
+    </div>
+
+    <h3 class='fs-4'>Client: {{$project->client}}</h2>
+    <h4>Typology: {{$project->typology}}</h4>
     <p>{{$project->resume}}</p>
 </div>
 @endsection
