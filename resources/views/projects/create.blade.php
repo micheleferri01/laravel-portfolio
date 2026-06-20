6d6beed486edf48b9ea1fa4ac09be0dd40a02c0b
micheleferri01 <micheleferri095@gmail.com>
2026-06-20 04:04:48 +0200

    add: view for route create of  route projects

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="mt-3">Add new project</h1>
+<form action='{{route("projects.store")}}' method="post">
+    @csrf
+    <div class="mt-2">
+        <label for="name" class="form-label">Project's name</label>
+        <input type="text" name="name" id="name" class="form-control">
+    </div>
+    <div class="mt-2">
+        <label for="author" class="form-label">Author</label>
+        <input type="text" name="author" id="author" class="form-control">
+    </div>
+    <div class="mt-2">
+        <label for="client" class="form-label">Client</label>
+        <input type="text" name="client" id="client" class="form-control">
+    </div>
+    <div class="mt-2">
+        <label for="typology" class="form-label">Typology</label>
+        <select name="typology" id="typology" class="form-control"></select>
+    </div>
+    <div class="mt-2">
+        <label for="resume" class="form-label">Project's description</label>
+        <textarea name="resume" id="resume" class="form-control"></textarea>
+    </div>
+    <div class="mt-3">
+        <input type="submit" value="Save" class="btn btn-primary">
+        <a href="{{route(projects.index)" class="btn btn-danger">Cancel</a>
+    </div>
+
+</form>
+@endsection
