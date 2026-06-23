66f4ba0aced2f2257af3c41833ad288a13df7614
micheleferri01 <micheleferri095@gmail.com>
2026-06-23 20:13:29 +0200

    switched column typology with column foreign key typology_id

---
+++ database/migrations/2026_06_23_150758_add_typology_id_to_table_projects.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('typology');
+            $table->foreignId('typology_id')->default(1)->after('resume')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_typology_id_foreign');
+            $table->dropColumn('typology_id');
+            $table->string('typology')->after('resume');
+        });
+    }
+};
