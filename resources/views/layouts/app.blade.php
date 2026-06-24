525f9c692f5c19b929a5e1d6b94a3dbed00cce6f
micheleferri01 <micheleferri095@gmail.com>
2026-06-24 22:15:13 +0200

    add: links to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,12 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/projects') }}">{{ __('Projects') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/typologies') }}">{{ __('Typologies') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
