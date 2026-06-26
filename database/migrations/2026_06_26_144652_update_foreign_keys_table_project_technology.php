fbf06fe39a45e5cfb222f36b8fa08a7d51e24a14
micheleferri01 <micheleferri095@gmail.com>
2026-06-26 17:27:26 +0200

    update: foreign keys of all tables

---
+++ database/migrations/2026_06_26_144652_update_foreign_keys_table_project_technology.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('project_technology', function (Blueprint $table) {
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['technology_id']);
+
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('technology_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('project_technology', function (Blueprint $table) {
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['technology_id']);
+
+            $table->foreignId('project_id')->constrained();
+            $table->foreignId('technology_id')->constrained();
+        });
+    }
+};
